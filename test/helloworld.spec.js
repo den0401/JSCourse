e9ab1020e3b3c8b5c039b0ba84f15426af8714dd
Denis Bury <db-itransition@jato.com>
2020-04-16 15:45:33 +0300

    To old version

--- test/helloworld.spec.js
+++
@@ -1,8 +0,0 @@
-const { describe, it } = require('mocha');
-const logger = require('../utils/log.utils');
-
-describe('Hello World TestSuite', () => {
-    it('should write "Hello World"', () => {
-        logger.info('Hello World');
-    });
-});
